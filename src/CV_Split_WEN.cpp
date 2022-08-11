14c80c82dd820d875f56c670ca3a9153c4594dbe
Kenbrown3 <78091862+Kenbrown3@users.noreply.github.com>
2022-08-10 19:58:23 -0700

    Update CV_Split_WEN.cpp

--- src/CV_Split_WEN.cpp
+++ src/CV_Split_WEN.cpp
@@ -594,9 +594,9 @@ double CV_Split_WEN::Logistic_Deviance(arma::mat x, arma::vec y,
     (std::exp(1)/(std::exp(1)-1))*
          (-2*arma::log(
            (1+(2*y-1))*arma::mean(y)/4+(1-(2*y-1))*(1-arma::mean(y))/4)%
-          (2*(2*y-1)%
+          arma::pow((2*(2*y-1)%
            arma::pow(
-             (2*y-1)+2*(arma::mean(intercept) + x * arma::mean(betas, 1))-1,-1))%
+             (2*y-1)+2*(arma::mean(intercept) + x * arma::mean(betas, 1))-1,-1)),1/(1-arma::mean(y))%
           (
             arma::exp(
               (-2*y+1)%(arma::mean(intercept) + x * arma::mean(betas, 1))
